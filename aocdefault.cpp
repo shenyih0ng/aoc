83bd8b3cacd3c60cd3d77408ebb9510cb9541dac
shenyih0ng <shen_yi_hong@outlook.com>
2021-01-07 16:14:27 +0800

    implemented file ext check for get_input_file_path, componentize handheld_halting logic

--- aocdefault.cpp
+++ aocdefault.cpp
@@ -11,6 +11,11 @@ std::string get_input_file_path (char* argv[]) {
 
 	int idx_last_split = bin_path.find_last_of("/\\");
 	std::string base_name = bin_path.substr(idx_last_split + 1);
+	int idx_ext = base_name.find_last_of(".");
+	if (idx_ext > -1) {
+		// ext present
+		base_name = base_name.substr(0, idx_ext);
+	}
 
 	return "./" + base_name + ".txt";
 }
--- handheld_halting.cpp
+++ handheld_halting.cpp
@@ -23,6 +23,7 @@ bool add_n_lines_to_cache (int n_lines, ifstream &stream, vector<pair<string, in
 			cache.push_back(make_pair(line, 0));
 			count++;
 		} else {
+			cout << "EOF." << endl;
 			eof = true;
 		}
 	}
@@ -40,6 +41,39 @@ pair<string, bool> retrieve_op (int op_num, vector<pair<string, int>> &cache) {
 	return make_pair(op_pair.first, true);
 }
 
+void run (ifstream &input_stream, vector<pair<string, int>> &cache, int &op_num, int &acc) {
+	bool done = false;
+	while (!done) {
+		if (op_num == -1) {
+			done = add_n_lines_to_cache(1, input_stream, cache);
+			op_num++;
+		}
+		pair<string, bool> op = retrieve_op(op_num, cache);
+		if (op.second == true) {
+			string op_tag = op.first.substr(0,3);
+			int acc_value = stoi(op.first.substr(4));
+
+			int lines_to_add = 0;
+			int op_num_increment = 1;
+			if (op_tag == "jmp") {
+				op_num_increment = acc_value;	
+			} else if (op_tag == "acc") {
+				acc += acc_value;
+			}
+			op_num += op_num_increment;
+			if (op_num >= cache.size()) {
+				lines_to_add = op_num - cache.size() + 1;
+			}
+
+			done = add_n_lines_to_cache(lines_to_add, input_stream, cache);
+		} else {
+			cout << "[STOPPED] Operation accessed twice!" << endl;
+			cout << "\tOp: " << op.first << " Op_Num: " << op_num << endl;
+			done = true;
+		}
+	}
+}
+
 int main (int argc, char *argv[]) {
 	ifstream input_stream(get_input_file_path(argv));
 
@@ -47,38 +81,18 @@ int main (int argc, char *argv[]) {
 	int op_num = -1;
 	vector<pair<string, int>> cache;
 	if (input_stream.is_open()) {
-		bool done = false;
-		while (!done) {
-			if (op_num == -1) {
-				done = add_n_lines_to_cache(1, input_stream, cache);
-				op_num++;
-			}
-			pair<string, bool> op = retrieve_op(op_num, cache);
-			if (op.second == true) {
-				string op_tag = op.first.substr(0,3);
-				int acc_value = stoi(op.first.substr(4));
-
-				int lines_to_add = 0;
-				int op_num_increment = 1;
-				if (op_tag == "jmp") {
-					op_num_increment = acc_value;	
-				} else if (op_tag == "acc") {
-					acc += acc_value;
-				}
-				op_num += op_num_increment;
-				if (op_num >= cache.size()) {
-					lines_to_add = op_num - cache.size() + 1;
-				}
-
-				done = add_n_lines_to_cache(lines_to_add, input_stream, cache);
-			} else {
-				done = true;
-			}
-		}	
+		// Part 1
+		run(input_stream, cache, op_num, acc);
+		cout << "Acc Value: " << acc << endl;
+		//display_cache(cache);
+		
+		// Part 2
+		//  - Use a single stream without reinitialization
+		//  - It probably has to be a iterative search for all the JMP to see which one corrupts the flow. Thats why part 1 is to implement a stop condition when it starts the infinite loop
+		
+	} else {
+		cout << "Stream is not opened!" << endl;
 	}
-	
-	display_cache(cache);
-	cout << acc << endl;
 
 	return 1;
 }
