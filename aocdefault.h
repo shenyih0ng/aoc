ee792c0eda205716e86783a18fd43f7cffad3bb5
shenyih0ng <shen_yi_hong@outlook.com>
2021-03-27 19:14:04 +0800

    added operator<< overload for vector class

--- aocdefault.h
+++ aocdefault.h
@@ -1,1 +1,19 @@
+#include <vector>
+#include <fstream>
+#include <iterator>
+
+template<typename T>
+std::ostream& operator<< (std::ostream& out, const std::vector<T>& v) {
+	if (!v.empty()) {
+		out << "[";
+		copy(v.begin(), v.end(), std::ostream_iterator<T>(out, ", "));
+		out << "\b\b]";
+	} else {
+		out << "[]";
+	}
+
+	return out;
+}
+
+// helper to get data file
 std::string get_input_file_path (int argc, char* argv[]);
