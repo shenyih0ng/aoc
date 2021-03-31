2aed9aaf3fead391f085b01ae1679b7be0a6e0ef
shenyih0ng <shen_yi_hong@outlook.com>
2021-03-31 20:06:27 +0800

    Completed Day-19 monster message, further improvements to be made for part2

--- Day-19/monster_messages.cpp
+++ Day-19/monster_messages.cpp
@@ -1,26 +1,15 @@
-#include <set>
+#include <regex>
+#include <climits>
 #include <sstream>
 #include <iostream>
-#include <algorithm>
 #include <unordered_map>
 
 #include <aocdefault.h>
 
 using namespace std;
 
-template<typename T>
-ostream& operator<< (ostream& out, set<T>& s) {
-	typename set<T>::iterator sIt = s.begin();
-	out << "[";
-	for (; sIt != s.end(); sIt++) {
-		out << *sIt << ' ';
-	}
-	out << "]";
-
-	return out;
-}
-
 struct Rule {
+	int idx;
 	char to_match;
 	vector<vector<int>> children;
 
@@ -73,58 +62,47 @@ bool Rule::is_leaf() {
 	return children.empty();
 }
 
-set<string> combine (set<string> base, set<string> to_add) {
-	set<string> combined;
-	if (!base.empty()) {
-		set<string>::iterator bIt = base.begin();
-		for (; bIt != base.end(); ++bIt) {
-			string bStr = *bIt;
-			set<string>::iterator aIt = to_add.begin();
-			for (; aIt != to_add.end(); ++aIt) {
-				string aStr = *aIt;
-				string cStr = bStr + aStr;
-				combined.insert(cStr);
-			}
-		}
-	} else {
-		combined.insert(to_add.begin(), to_add.end());
+string build_regex (Rule* qRule, unordered_map<size_t, Rule*>& rules, size_t depth = 15) {
+	if (depth == 0) {
+		// force stop as it reaches maximum depth
+		return "";
 	}
 
-	return combined;
-}
-
-
-set<string> get_rule_matches (Rule* qRule, unordered_map<size_t, Rule*>& rules) {
 	if (qRule->is_leaf()) {
-		set<string> result;
-		
-		string _s = {qRule->to_match};	
-		result.insert(_s);
-
-		return result;
+		return {qRule->to_match};
 	} else {
-		set<string> all_matches;
+		string rgx;
+		rgx += "(";
 		for (int sIdx = 0; sIdx < qRule->children.size(); sIdx++) {
 			vector<int> child = qRule->children[sIdx];
 
-			set<string> section_matches;
+			if (sIdx) rgx+= "|";
 			for (int cIdx = 0; cIdx < child.size(); cIdx++) {
-				set<string> child_match = get_rule_matches(rules[child[cIdx]], rules);
-				section_matches = combine(section_matches, child_match);
+				rgx += build_regex(rules[child[cIdx]], rules, depth-1);
 			}
-			all_matches.insert(section_matches.begin(), section_matches.end());
 		}
-		return all_matches;
+		rgx += ")";	
+		return rgx;
 	}
 }
 
+size_t get_matches (string rgx, vector<string>& msgs) {
+	smatch _m;
+	regex re(rgx);
+	size_t matches = 0;
+	for (int idx = 0; idx < msgs.size(); idx++) {
+		matches += regex_match(msgs[idx], _m, re);
+	}
+	
+	return matches;	
+}
 
 int main (int argc, char* argv[]) {
 	ifstream input_stream(get_input_file_path(argc, argv));
 
 	if (input_stream.is_open()) {
 		unordered_map<size_t, Rule*> rules;
-		set<string> msgs;
+		vector<string> msgs;
 
 		string line;
 		bool rule_end = false;
@@ -137,16 +115,23 @@ int main (int argc, char* argv[]) {
 				int colon_sep_pos = line.find_first_of(':');
 				int rule_idx = atoi(line.substr(0, colon_sep_pos).c_str());
 				Rule* n_rule = new Rule(line.substr(colon_sep_pos + 1));
+				n_rule->idx = rule_idx;
 				rules[rule_idx] = n_rule;
 			} else if (line != "") {
-				msgs.insert(line);
+				msgs.push_back(line);
 			}
 		}
-		set<string> matches = get_rule_matches(rules[0], rules);
+		// Part1	
+		string rgx = build_regex(rules[0], rules, INT_MAX);
+		cout << "[p1] matches: " << get_matches(rgx, msgs) << endl;
 
-		set<string> intersect;
-		set_intersection(msgs.begin(), msgs.end(), matches.begin(), matches.end(), inserter(intersect, intersect.begin()));
-		cout << "isize: " << intersect.size() << endl;	
+		// Part 2
+		vector<int> new_rule_8 {42,8};
+		vector<int> new_rule_11 {42,11,31};
+		rules[8]->children.push_back(new_rule_8);
+		rules[11]->children.push_back(new_rule_11);
+		string rgx_2 = build_regex(rules[0], rules, 15);
+		cout << "[p2] matches: " << get_matches(rgx_2, msgs) << endl;
 	}
 
 	return 0;
