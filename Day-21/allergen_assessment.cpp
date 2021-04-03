356a1446254f23e429768f708908a915699242ea
shenyih0ng <shen_yi_hong@outlook.com>
2021-04-03 15:33:12 +0800

    completed d21p2

--- Day-21/allergen_assessment.cpp
+++ Day-21/allergen_assessment.cpp
@@ -1,4 +1,5 @@
 #include <set>
+#include <map>
 #include <sstream>
 #include <algorithm>
 #include <unordered_map>
@@ -6,6 +7,17 @@
 #include <aocdefault.h>
 
 using namespace std;
+
+// UTILITIES:
+
+ostream& operator<< (ostream& out, map<string, set<string>>& m) {
+	map<string, set<string>>::iterator sIt = m.begin();
+	for (; sIt != m.end(); sIt++) {
+		out<< sIt->first << sIt->second << endl;
+	}
+
+	return out;
+}
 
 set<string> to_set (string str, char delimiter) {
 	stringstream ss(str);
@@ -29,43 +41,55 @@ set<string> to_set (string str, char delimiter) {
  * @param found 		food with allergen found
  *
  */
-void step (set<string> foods, set<string> allergens, 
-		unordered_map<string, set<string>>& allergen_map, set<string>& found) {
+void step (set<string> foods, 
+		set<string> allergens, 
+		map<string, set<string>>& allergen_map, 
+		set<string>& found) {
+	int initial_num_found = found.size();
 	set<string>::iterator aIt = allergens.begin();
 	for (;aIt != allergens.end(); aIt++) {
-		unordered_map<string, set<string>>::iterator allergen_set = allergen_map.find(*aIt);
+		map<string, set<string>>::iterator allergen_set = allergen_map.find(*aIt);
 		if (allergen_set == allergen_map.end()) {
 			allergen_map[*aIt] = foods;	
-		} else {
+		} else if (allergen_set->second.size() > 1) {
 			// find intersection of new and current set
 			set<string> intersect;
 			set_intersection(foods.begin(), foods.end(), 
 					allergen_set->second.begin(), allergen_set->second.end(), 
 					inserter(intersect, intersect.begin()));
-			// remove food with allergen found
-			set<string> diff;
-			set_difference(intersect.begin(), intersect.end(),
-					found.begin(), found.end(),
-					inserter(diff, diff.begin()));
 
-			if (diff.size() == 1) {
-				// check if we have found the food with the allergen
-				found.insert(diff.begin(), diff.end());
+			if (intersect.size() == 1) {
+				found.insert(intersect.begin(), intersect.end());
+				allergen_map[*aIt] = intersect;
+			} else {				
+				// remove food that already has its allergen found from current set
+				set<string> diff;
+				set_difference(intersect.begin(), intersect.end(),
+						found.begin(), found.end(),
+						inserter(diff, diff.begin()));
+				if (diff.size() == 1) {
+					found.insert(diff.begin(), diff.end());
+				}
+				allergen_map[*aIt] = diff;
 			}
-			allergen_map[*aIt] = diff;
 		}
 	}
-
-	unordered_map<string, set<string>>::iterator amIt = allergen_map.begin();
-	for (;amIt != allergen_map.end(); amIt++) {
-		set<string> _diff;
-		set_difference(amIt->second.begin(), amIt->second.end(),
-				found.begin(), found.end(),
-				inserter(_diff, _diff.begin()));
-		if (_diff.size() == 1) {
-			found.insert(_diff.begin(), _diff.end());
+	
+	if (initial_num_found < found.size()) {
+		// update map if number of food with allergen found increased
+		map<string, set<string>>::iterator amIt = allergen_map.begin();
+		for (;amIt != allergen_map.end(); amIt++) {
+			if (amIt->second.size() > 1) {
+				set<string> _diff;
+				set_difference(amIt->second.begin(), amIt->second.end(),
+						found.begin(), found.end(),
+						inserter(_diff, _diff.begin()));	
+				if (_diff.size() == 1) {
+					found.insert(_diff.begin(), _diff.end());
+				}
+				amIt->second = _diff;
+			}
 		}
-		amIt->second = _diff;
 	}
 }
 
@@ -78,7 +102,7 @@ int main (int argc, char* argv[]) {
 		set<string> found;
 		set<string> all_foods;
 		unordered_map<string, int> food_count;
-		unordered_map<string, set<string>> allergen_sets;
+		map<string, set<string>> allergen_sets;
 		while(getline(input_stream, line)) {
 			int apos = line.find_first_of('(');
 			int epos = line.find_last_of(')');
@@ -97,6 +121,7 @@ int main (int argc, char* argv[]) {
 			step(fset, aset, allergen_sets, found);
 		}
 		
+		// Part 1	
 		set<string> no_allergens;
 		set_difference(all_foods.begin(), all_foods.end(),
 				found.begin(), found.end(),
@@ -108,5 +133,15 @@ int main (int argc, char* argv[]) {
 			appearance_count += food_count[*it];
 		}
 		cout << "acount: " << appearance_count << endl;
+		
+		// Part 2
+		map<string, set<string>>::iterator aIt = allergen_sets.begin();
+		for (; aIt != allergen_sets.end(); aIt++) {
+			if (distance(allergen_sets.begin(), aIt) > 0) {
+				cout << ',';
+			}
+			cout << *(aIt->second.begin());
+		}
+		cout << endl;	
 	}
 }
