1fdfce61a502b94c0e0f335b1c9edf49c43469d2
shenyih0ng <shen_yi_hong@outlook.com>
2021-04-04 17:30:09 +0800

    completed d22p2 - improvements to be made to exec time down

--- Day-22/crab_combat.cpp
+++ Day-22/crab_combat.cpp
@@ -1,38 +1,94 @@
+#include <map>
+#include <assert.h>
+#include <algorithm>
+
 #include <aocdefault.h>
 
 using namespace std;
 
-bool has_winner (vector<int>& p1, vector<int>& p2) {
-	return p1.size() == 0 || p2.size() == 0;
-}
-
-int calc_winner_score(vector<int>& p1, vector<int>& p2) {
-	vector<int>& winner = (p1.size() != 0) ? p1 : p2;
-	int size = winner.size();
-
+int calc_score(vector<int>& p) {
 	int score = 0;
-	for (; size > 0; size--) {
-		score += winner[winner.size()-size] * size;
+	for (int size = p.size(); size > 0; size--) {
+		score += p[p.size()-size] * size;
 	}
 
 	return score;
 }
 
-void play (vector<int>& p1, vector<int>& p2) {
-	while (!has_winner(p1, p2)) {
-		int p1_c = p1[0];
-		p1.erase(p1.begin());
-		int p2_c = p2[0];
-		p2.erase(p2.begin());
+bool has_winner (vector<int>& p1, vector<int>& p2) {
+	return p1.size() == 0 || p2.size() == 0;
+}
+	
+bool if_subgame (vector<int>& p1, vector<int>& p2) {
+	return ((p1.size()-1) >= p1[0]) && ((p2.size()-1) >= p2[0]);
+}
 
-		if (p1_c > p2_c) {
-			p1.push_back(p1_c);
-			p1.push_back(p2_c);
-		} else {
-			p2.push_back(p2_c);
-			p2.push_back(p1_c);
-		}
+bool if_repeated_hand (vector<int>& p1, vector<int>& p2, 
+		vector<vector<int>>& prev_p1, vector<vector<int>>& prev_p2) {
+	assert(prev_p1.size() == prev_p2.size());
+	
+	vector<vector<int>>::iterator p1_found = find(prev_p1.begin(), prev_p1.end(), p1);	
+	vector<vector<int>>::iterator p2_found = find(prev_p2.begin(), prev_p2.end(), p2);	
+	if (p1_found != prev_p1.end() && p2_found != prev_p2.end()) {
+		return distance(p1_found, prev_p1.begin()) == distance(p2_found, prev_p2.begin());
 	}
+
+	return false;
+}
+
+// w recursive combat
+bool play (vector<int>& p1, vector<int>& p2,
+		vector<vector<int>>& prev_p1, vector<vector<int>>& prev_p2) {
+	while (!has_winner(p1,p2) && !if_repeated_hand(p1, p2, prev_p1, prev_p2)) {
+		prev_p1.push_back(p1);
+		prev_p2.push_back(p2);
+
+		bool p1_round_win;
+		if (if_subgame(p1, p2)) {
+			vector<int> sub_p1;
+			copy(p1.begin() + 1, p1.begin() + p1[0] + 1, back_inserter(sub_p1));
+			vector<int> sub_p2;
+			copy(p2.begin() + 1, p2.begin() + p2[0] + 1, back_inserter(sub_p2));
+
+			vector<vector<int>> sub_prev_p1, sub_prev_p2;
+			p1_round_win = play(sub_p1, sub_p2, sub_prev_p1, sub_prev_p2);
+		} else {
+			p1_round_win = p1[0] > p2[0];			
+		}
+
+		if (p1_round_win) {
+			p1.push_back(p1[0]);
+			p1.push_back(p2[0]);
+		} else {
+			p2.push_back(p2[0]);
+			p2.push_back(p1[0]);
+		}
+		p1.erase(p1.begin());
+		p2.erase(p2.begin());
+	}
+	
+	if (!has_winner(p1,p2)) {
+		return true;
+	}
+
+	return (p1.size() != 0);
+}
+
+// w/o recursive combat
+bool play (vector<int>& p1, vector<int>& p2) {
+	while (!has_winner(p1, p2)) {
+		if (p1[0] > p2[0]) {
+			p1.push_back(p1[0]);
+			p1.push_back(p2[0]);
+		} else {
+			p2.push_back(p2[0]);
+			p2.push_back(p1[0]);
+		}
+		p1.erase(p1.begin());
+		p2.erase(p2.begin());
+	}
+
+	return (p1.size() != 0);
 }
 
 int main (int argc, char* argv[]) {
@@ -55,8 +111,22 @@ int main (int argc, char* argv[]) {
 				_p2 = true;
 			}
 		};
-		play(p1, p2);
-		cout << "wscore: " << calc_winner_score(p1, p2) << endl;
+		vector<int> p1_copy = p1;
+		vector<int> p2_copy = p2;
+
+		//Part 1
+		bool part1_p1_win = play(p1, p2);
+		cout << "[part1]" << endl;
+		cout << ((part1_p1_win) ? "p1 wins!" : "p2 wins!");
+		cout << " wscore: " << calc_score((part1_p1_win)? p1 : p2) << endl;
+		
+		//Part 2	
+		vector<vector<int>> prev_p1;
+		vector<vector<int>> prev_p2;
+		bool part2_p1_win = play(p1_copy, p2_copy, prev_p1, prev_p2);
+		cout << "[part2]" << endl;
+		cout << ((part2_p1_win) ? "p1 wins!" : "p2 wins!");
+		cout << " wscore: " << calc_score((part2_p1_win)? p1_copy : p2_copy) << endl;
 	}
 
 	return 0;
