dec5280f01fd27fdb4f49d5112efb63509d537f2
shenyih0ng <shen_yi_hong@outlook.com>
2021-04-10 14:59:52 +0800

    completed d24p2 - improvements to be made

--- Day-24/lobby_layout.cpp
+++ Day-24/lobby_layout.cpp
@@ -21,7 +21,12 @@ pair<float, float>& operator+= (pair<float, float>& lhs, pair<float, float>& rhs
 	return lhs;
 }
 
+pair<float, float> operator+ (pair<float, float> lhs, pair<float, float> rhs) {
+	return make_pair(lhs.first + rhs.first, lhs.second + rhs.second);
+}
+
 const regex rgx("e|se|sw|w|nw|ne");
+const char* adjacent[] = {"e", "se", "sw", "w", "nw", "ne"};
 
 pair<float, float> decode_step (string step) {
 	assert(step.size() <= 2);
@@ -55,9 +60,46 @@ pair<float, float> find_displacement(string steps) {
 	return displ_2d;
 }
 
+void step (map<pair<float, float>, bool>& tiles, vector<pair<float, float>>& black_tiles) {
+	map<pair<float, float>, bool> updated_tiles;
+	vector<pair<float, float>> updated_black_tiles;
+
+	map<pair<float, float>, int> white_tiles;
+	
+	vector<pair<float, float>>::const_iterator bIt = black_tiles.begin();
+	for (; bIt != black_tiles.end(); bIt++) {
+		int num_black_adj = 0;
+		for (int idx = 0; idx < 6; idx++) {
+			pair<float, float> adj_pos = (*bIt) + decode_step(adjacent[idx]);
+			num_black_adj += tiles[adj_pos];
+			if (!tiles[adj_pos]) {
+				white_tiles[adj_pos] += 1;
+			}
+		}
+
+		if (!(num_black_adj == 0 || num_black_adj > 2)) {
+			updated_tiles[*bIt] = true;
+			updated_black_tiles.push_back(*bIt);
+		}
+	} 
+
+	map<pair<float, float>, int>::const_iterator wIt = white_tiles.begin();
+	for (; wIt != white_tiles.end(); wIt++) {
+		if (wIt->second == 2) {
+			pair<float, float> w_pos = wIt->first;
+			updated_black_tiles.push_back(w_pos);
+			updated_tiles[w_pos] = true;
+		}
+	}
+
+	// update state
+	tiles = updated_tiles;
+	black_tiles = updated_black_tiles;
+}
+
 int main (int argc, char* argv[]) {
 	ifstream input_stream(get_input_file_path(argc, argv));
-	
+
 	if (input_stream.is_open()) {
 		string steps;
 		int num_black = 0;
@@ -66,8 +108,24 @@ int main (int argc, char* argv[]) {
 		while(getline(input_stream, steps)) {
 			pair<float, float> displ_2d = find_displacement(steps);
 			tiles[displ_2d] = !tiles[displ_2d];
-			num_black += (tiles[displ_2d] == false) ? -1 : 1;
+			num_black += (tiles[displ_2d]) ? 1 : -1;
 		}
 		cout << "[p1] " << num_black << endl;	
+
+		// Part 2
+		vector<pair<float, float>> black_tiles;
+		map<pair<float, float>, bool>::const_iterator mIt = tiles.begin();
+		for (; mIt != tiles.end(); mIt++) {
+			if (mIt->second) {
+				black_tiles.push_back(mIt->first);
+			}
+		}
+
+		int count = 100;
+		while (count > 0) {
+			step(tiles, black_tiles);
+			count--;
+		}
+		cout << "[p2] " << black_tiles.size() << endl;
 	};
 }
